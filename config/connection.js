576e8a83ef1d4c38e98a916a2dde26913ec18836
or1979 <orobinson79@gmail.com>
2019-04-24 10:36:59 -0400

    herokuconnect

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 
 var connection;
-if (process.env.PORT) {
+if (process.env.JAWSDB_URL) {
     
-    connection = mysql.createConnection(process.env.PORT);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     
     connection = mysql.createConnection({
         port: 3306,
         host: "localhost",
         user: "root",
-        password: "",
+        password: "oBrcLt16!",
         database: "burger_db"
     });
 }
